696d1d51ed81c3d1da09e796227bbb4baab82cd8
taimik123 <taimi.lulu@gmail.com>
2023-05-30 10:16:58 -0700

    updated handle function

--- handle_print.c
+++ handle_print.c
@@ -15,7 +15,7 @@ int handle_print(const char *fmt, int *index, va_list list1, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int k, unknow_len = 0, printed_chars = -1;
-	fmt_ty fmt_types[] = {
+	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
