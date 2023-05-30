19d5609cb1bd57e16dc9a5c8a17d88d25d387810
taimik123 <taimi.lulu@gmail.com>
2023-05-30 07:10:05 -0700

    updated handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,48 +1,49 @@
 #include "main.h"
 /**
- * handle_print - this Prints the provided argument based on its type
- * @fmt112: this is The formatted string used to print the arguments
- * @list: ths is the list of arguments
- * @ind: an indicator var
- * @buffer: this is a Buffer array to handle print.
- * @flags:they are able to calculate active flags
- * @width: for gettng width.
- * @precision: this is Precision specifier
- * @size: Size specifie coder
+ * handle_print - Prints an argument based on its type
+ * @fmt: code for Formttd str to print the arguments.
+ * @list1:this is jst  List1 of arguments to be printed.
+ * @ind: index is defined.
+ * @buffer:code for Buffer array to handle print.
+ * @flags: this code Calculates active flags
+ * @width: code to get width.
+ * @precision: code for Precision specification
+ * @size: Size specifier code
+ * @k: an index
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt112, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *ind, va_list list1, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int k, unknow_len = 0, printed_chars = -1;
-	fmt_t112 fmt112_types[] = {
+	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (k = 0; fmt112_types[k].fmt112 != '\0'; k++)
-		if (fmt112[*ind] == fmt112_types[k].fmt112)
-			return (fmt112_types[k].fn112(list, buffer, flags, width, precision, size));
+	for (k = 0; fmt_types[k].fmt != '\0'; k++)
+		if (fmt[*ind] == fmt_types[k].fmt)
+			return (fmt_types[k].fn(list1, buffer, flags, width, precision, size));
 
-	if (fmt112_types[k].fmt112 == '\0')
+	if (fmt_types[k].fmt == '\0')
 	{
-		if (fmt112[*ind] == '\0')
+		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt112[*ind - 1] == ' ')
+		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
-			while (fmt112[*ind] != ' ' && fmt112[*ind] != '%')
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (fmt112[*ind] == ' ')
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
-		unknow_len += write(1, &fmt112[*ind], 1);
+		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
 	return (printed_chars);
